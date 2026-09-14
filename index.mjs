737d4bc229fe67ac80edd30b71cf882f8e940423
Maq897 <sonofsheik@gmail.com>
2026-09-14 18:20:37 +0400

    s

--- index.mjs
+++ index.mjs
@@ -15,7 +15,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: ["http://127.0.0.1:5500"]}))
+cors({ origin: ["http://127.0.0.1:5500", "http://localhost:5500"] })
 // Logger FIRST
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
@@ -148,3 +148,4 @@ await mongodbloader();
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
